7bb37cd12df57c150fc47cc50fc229ff2d85825d
gand0rf <gand0rf@protonmail.com>
2026-02-16 21:29:38 -0500

    Issue running code on laptop in VSCode. Ran fine in terminal.

--- sys_scan.py
+++ sys_scan.py
@@ -4,7 +4,8 @@ import modules.hub as hub
 import os
 
 if __name__ == "__main__":
-    user = os.getlogin()
+    #user = os.getlogin() ## Not working in VSCode 1.109.4 Using alternative
+    user = os.environ.get('USER')
     acc_pass = input(f'If you have the current accounts password for {user}, please enter it: ')
     results = hub.tests.full_run(acc_pass)
     print(results)
