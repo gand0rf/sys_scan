895c98f0edbcc7e4d95b63fa35fef6fd82b5ec53
gand0rf <gand0rf@protonmail.com>
2026-02-18 21:09:19 -0500

    Updated to save results in text file and adjusted teh printed message to tell user.

--- sys_scan.py
+++ sys_scan.py
@@ -2,10 +2,18 @@
 
 import modules.hub as hub
 import os
+import socket
 
 if __name__ == "__main__":
     #user = os.getlogin() ## Not working in VSCode 1.109.4 Using alternative
     user = os.environ.get('USER')
     acc_pass = input(f'If you have the current accounts password for {user}, please enter it: ')
     results = hub.tests.full_run(acc_pass)
-    print(results)
+
+    # Write results out to a file
+    hostname = socket.gethostname()
+    with open(f'{hostname}-scan.txt', 'w') as f:
+        f.writelines(results)
+
+    # Let user know scan is completed and whre to find the FileExistsError
+    print(f'\nScan comleted. Results are saved in {hostname}-scan.txt file\n')
